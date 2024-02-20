1a2da0c9ef8a4a1f8fbb2cdd3f0a112713725133
ozen-hub <info.ozenhub@gmail.com>
2024-02-20 23:26:18 +0530

    14

--- src/app/components/dashboard/dashboard/inner-items/discount/discount-default/discount-default.component.ts
+++ src/app/components/dashboard/dashboard/inner-items/discount/discount-default/discount-default.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatPaginator} from "@angular/material/paginator";
+import {NgForOf} from "@angular/common";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CategoryService} from "../../../../../services/category.service";
+import {DiscountsService} from "../../../../../services/discounts.service";
 
 @Component({
   selector: 'app-discount-default',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButton,
+        MatPaginator,
+        NgForOf,
+        ReactiveFormsModule
+    ],
   templateUrl: './discount-default.component.html',
   styleUrl: './discount-default.component.scss'
 })
-export class DiscountDefaultComponent {
+export class DiscountDefaultComponent implements AfterViewInit, OnInit{
+
+  page: any = 0;
+  size: any = 10;
+  dataArray: any[] = [];
+  count = 0;
+
+  constructor( private discountsService: DiscountsService) {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+
+  private loadData() {
+    this.discountsService.loadData(this.page, this.size).subscribe(response => {
+      console.log(response);
+      this.dataArray = response?.dataList;
+      this.count = response?.count;
+    })
+  }
+
+  getServerData(pageData: any) {
+    this.page = pageData.pageIndex;
+    this.size = pageData.pageSize;
+    this.loadData();
+  }
+
+  showProduct(id: any) {
+    // need to implement the code
+  }
 
 }
